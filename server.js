7367220deb4e66d840ae4ba9051c43c3ab3ba11c
Virginia Wilcox <wilcoxva@gmail.com>
2021-12-21 22:27:48 -0500

    added helmet

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 if (process.env.NODE_ENV !== 'production') { 
 require('dotenv').config() }
 const express = require("express");
-const app = express();
+const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const itemsRouter = require("./routes/items-router");
 
-app.use(express.json());
+const app = express();
 
 app.use(cors());
+
+app.use(express.json());
 
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(bodyParser.json());
+
+app.use(helmet());
 
 app.use('/', itemsRouter);
 
