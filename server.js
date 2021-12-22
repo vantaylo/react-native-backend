bb0d10e005692f2c24d9c8ecd29181848b2f37a7
Virginia Wilcox <wilcoxva@gmail.com>
2021-12-21 22:22:12 -0500

    changed error handling

--- server.js
+++ server.js
@@ -19,10 +19,16 @@ app.use('/', itemsRouter);
 
 app.listen(process.env.PORT || 3000);
 
-app.use((err, req, res, next) => {
-	res.status(500).json({
-		message: "Something went wrong",
-	});
+app.use((req, res, next) => {
+	next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app
