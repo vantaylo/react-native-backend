265501a254549e3d173a055ee7a381730a1b5184
Virginia Wilcox <wilcoxva@gmail.com>
2021-12-22 14:48:13 -0500

    made development connect to elephantsql db

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,7 @@ module.exports = {
 
   development: {
     client: "pg",
-    connection: {
-      host: process.env.HOST,
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
-      charset: "utf8"
-    },
+    connection: process.env.DB_URL,
     migrations: {
       directory: __dirname + "/knex/migrations",
     },
